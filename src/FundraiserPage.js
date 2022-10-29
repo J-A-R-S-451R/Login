21ba82a8a4b3d4a6fda80818660835815f5a4878
inlimbo <rex1324567@gmail.com>
2022-10-29 18:09:52 -0500

    Add CSS file for FundraiserPage

--- src/FundraiserPage.js
+++ src/FundraiserPage.js
@@ -1,3 +1,4 @@
+import "./FundraiserPage.css"
 import { LinearProgress, Paper, Typography } from '@mui/material';
 import { linearProgressClasses } from '@mui/material/LinearProgress';
 import { styled } from '@mui/material/styles';
@@ -18,26 +19,24 @@ const BorderLinearProgress = styled(LinearProgress)(({ theme }) => ({
 
 function FundraiserPage() {
     return (
-        <div className="donation-page" style={{display: "flex", flexDirection: "column", width: "98vw", justifyContent: "center", justifyItems: "center", alignItems: "center"}}>
-            <Paper sx={{ height: "75vh", width: "80vw", marginTop: "32px"}}>
-                <div style={{display: "flex", flexDirection: "row", justifyItems: "center", alignItems: "stretch", height: "100%"}}>
-                    <div style={{flex: "5"}}>
-                        <div className="content-area" style={{display: "flex", flexDirection: "column", height: "100%"}}>
-                            <div className="fundraiser-details" style={{padding: "32px 32px 0px 32px", overflowY: "auto", flexGrow: "1", boxShadow: "0px -4px 6px -5px inset rgb(0 0 0 / 50%)"}}>
+        <div className="donation-page">
+            <Paper className="paper-container">
+                <div className="page-contents">
+                    <div className="content-column">
+                        <div className="content-area">
+                            <div className="fundraiser-details">
                                 <Typography variant="h4">Fundraiser</Typography>
                                 <Typography variant="body1">Goal $7000</Typography>
                                 <Typography variant="body1">This is a fundraiser description.</Typography>
                             </div>
 
-
-
-                            <div style={{marginTop: "auto", boxShadow: "3px", padding: "16px", textAlign: "center"}}>
+                            <div className="progress-bar-container">
                                 Progress: $325 out of $500
-                                <BorderLinearProgress variant="determinate" sx={{height: "16px", marginTop: "6px"}} value={50} />
+                                <BorderLinearProgress className="progress-bar" variant="determinate" value={50} />
                             </div>
                         </div>
                     </div>
-                    <RecentDonations style={{flex: "1"}}></RecentDonations>
+                    <RecentDonations className="recent-donations-column"></RecentDonations>
                 </div>
             </Paper>
         </div>
