3f2a888c913d03d7f69910ac4460456e964a93ae
inlimbo <rex1324567@gmail.com>
2022-10-29 18:15:57 -0500

    Add DashboardPage.css

--- src/DashboardPage.js
+++ src/DashboardPage.js
@@ -1,3 +1,4 @@
+import './DashboardPage.css'
 import { Typography } from '@mui/material';
 import React from 'react';
 import FundraiserCard from './FundraiserCard';
@@ -9,14 +10,14 @@ function DashboardPage({ fundraisers }) {
   }
 
   return (
-      <div className="dashboard-container" style={{display: "flex", flexDirection: "column", width: "98vw", justifyContent: "center", justifyItems: "center", alignItems: "center"}}>
-        <div className="hero-header" style={{display: "flex", flowDirection: "column", justifyItems: "column", justifyContent: "center", alignItems: "center", width: "100vw", height: "300px", backgroundImage: "url(https://mymodernmet.com/wp/wp-content/uploads/2020/08/Story-Of-Flowers-Still-thumbnail.jpg)", backgroundSize: "cover"}}>
-          <Typography fontWeight={800} fontSize="32px" color="white"  sx={{textShadow: "black 1px 0 21px"}}>
+      <div className="dashboard-container">
+        <div className="hero-header">
+          <Typography className="hero-text" fontWeight={800} fontSize="32px" color="white">
             We all must work to make the world worthy
           </Typography>
         </div>
 
-        <div className="fundraiser-cards" style={{width: "50vw", display: "flex", flexDirection: "column", justifyContent: "center", justifyItems: "center"}}>
+        <div className="fundraiser-cards">
             {fundraisers.map(x => {
                 return <FundraiserCard fundraiser={x}></FundraiserCard>
             })}
